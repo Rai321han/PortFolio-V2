87cba1bb1b4da213f8a9521e7246569d53b5f50a
Raihan Uddin <uddinraihan797@gmail.com>
2025-03-04 01:47:42 +0600

    color changed

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -12,7 +12,7 @@ export default function Hero() {
       className="flex flex-col gap-5 items-center justify-center h-[100%] w-screen"
     >
       <div
-        className={`${amaticFont.className} font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-[#399ddc] to-[#000917] to-80%  text-9xl sm:text-[150px] md:text-[300px]`}
+        className={`${amaticFont.className} font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-linkHoverColor to-[#000917] to-80%  text-[120px] sm:text-[150px] md:text-[300px]`}
       >
         Raihan
       </div>
